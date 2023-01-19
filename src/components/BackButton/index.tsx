d598e3c87368fda1f55845f7d60e73e1c6071d3c
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-19 16:07:12 -0300

    feat: :sparkles: cria header e input pagina de novo habito

--- src/components/BackButton/index.tsx
+++ src/components/BackButton/index.tsx
@@ -6,8 +6,8 @@ import colors from 'tailwindcss/colors';
 function BackButton() {
   const { goBack } = useNavigation();
   return (
-    <TouchableOpacity onPress={goBack}>
-      <Feather name="arrow-left" size={24} color={colors.gray[100]} />
+    <TouchableOpacity onPress={goBack} activeOpacity={0.7}>
+      <Feather name="arrow-left" size={32} color={colors.zinc[400]} />
     </TouchableOpacity>
   );
 }
--- src/screens/HabitRegister/index.tsx
+++ src/screens/HabitRegister/index.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, View } from 'react-native';
+import { ScrollView, Text, TextInput, View } from 'react-native';
 import BackButton from '../../components/BackButton';
 
 function HabitRegisterPage() {
@@ -6,6 +6,22 @@ function HabitRegisterPage() {
     <View className={'flex-1 bg-background px-8 pt-16'}>
       <ScrollView showsVerticalScrollIndicator={false}>
         <BackButton />
+
+        <Text className="mt-6 text-white font-extrabold text-3xl">
+          Criar Hábito
+        </Text>
+
+        <Text className="mt-6 text-white font-smibold text-base">
+          Qual é o seu comprometimento?
+        </Text>
+
+        <TextInput
+          className={`
+          h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white
+          focus:border-2 border-violet-500 outline-none
+          `}
+          placeholder="Digite aqui"
+        />
       </ScrollView>
     </View>
   );
