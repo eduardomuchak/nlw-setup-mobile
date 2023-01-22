a62ac755c5a38309c30c59d3fd6d77ee0a799989
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-22 17:25:43 -0300

    build: :heavy_plus_sign: adiciona dependencia react native reanimated

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,16 +1,28 @@
+import { useEffect } from 'react';
 import { View } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
 
 interface Props {
   progress?: number;
 }
 
 export function ProgressBar({ progress = 0 }: Props) {
+  const sharedProgress = useSharedValue(progress);
+
+  const style = useAnimatedStyle(() => ({
+    width: `${sharedProgress.value}%`,
+  }));
+
+  useEffect(() => {
+    sharedProgress.value = progress;
+  }, [progress]);
+
   return (
     <View className="w-full h-3 rounded-xl bg-zinc-700 mt-4">
-      <View
-        className="h-3 rounded-xl bg-violet-600"
-        style={{ width: `${progress}%` }}
-      />
+      <Animated.View className="h-3 rounded-xl bg-violet-600" style={style} />
     </View>
   );
 }
